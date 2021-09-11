a2671da657d1fc6476e3a9033626c601478835dc
shruta.saralaya.prasad <shruta.saralaya.prasad@kdi.kongsberg.com>
2021-09-11 15:33:06 +0530

    first commit

---
+++ NumberOrderAPITests/Controllers/NumberOrderControllerTests.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using NumberOrderingAPI.Controllers;
+using NumberOrderingAPI.Services;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace NumberOrderAPITests.Controllers
+{
+    [TestClass]
+    public class NumberOrderControllerTests
+    {
+        private Mock<INumberOrderService> _mockNumberOrderService;
+        private NumberOrderController _controller;
+
+        [TestInitialize]
+        public void InitializeTestData()
+        {
+            _mockNumberOrderService = new Mock<INumberOrderService>();
+            _controller = new NumberOrderController(_mockNumberOrderService.Object);
+        }
+
+        [TestMethod]
+        public void NumberOrderController_Should_Be_Initialized()
+        {
+            Assert.IsNotNull(_controller);
+        }
+
+        [TestMethod]
+        public async Task StoreNumbersInOrder_Should_Return_400_StatusCode()
+        {
+            var response = await _controller.StoreNumbersInOrder(null) as ObjectResult;
+            Assert.AreEqual((int)HttpStatusCode.BadRequest, response.StatusCode);
+        }
+
+        [TestMethod]
+        public async Task StoreNumbersInOrder_Should_Return_200_StatusCode()
+        {
+            var unOreredList = new List<int>() { 100, 20, 50, 10 };
+            var orderedList = new List<int>() { 10, 20, 50, 100 };
+            _mockNumberOrderService.Setup(x => x.SortUnOrderedList(It.IsAny<List<int>>())).Returns(orderedList);
+            _mockNumberOrderService.Setup(x => x.StoreListIntoFile(It.IsAny<List<int>>())).Returns(Task.FromResult(true));
+            var response = await _controller.StoreNumbersInOrder(unOreredList) as ObjectResult;
+            Assert.AreEqual((int)HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [TestMethod]
+        public void GetLatestFileData_Should_Return_200()
+        {
+            var expectedResult = new List<string>() { "10", "20", "30"};
+            _mockNumberOrderService.Setup(x => x.GetLatestFileData()).Returns(expectedResult);
+            var response = _controller.GetLatestFileData();
+            Assert.IsNotNull(response);
+        }
+    }
+}
---
+++ NumberOrderAPITests/Services/NumberOrderServiceTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NumberOrderingAPI.Services;
+using System.Collections.Generic;
+
+namespace NumberOrderAPITests.Services
+{
+    [TestClass]
+    public class NumberOrderServiceTests
+    {
+        private NumberOrderService _service;
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            _service = new NumberOrderService();
+        }
+
+        [TestMethod]
+        public void TankConfigService_Shold_be_Initialized()
+        {
+            Assert.IsNotNull(_service);
+        }
+
+        [TestMethod]
+        public void SortUnOrderedList_Should_Return_SortedList()
+        {
+            var unOrderedList = new List<int>() { 100, 20, 50, 10 };
+            var expectedResult = new List<int>() { 10, 20, 50, 100 };
+
+            var result = _service.SortUnOrderedList(unOrderedList);
+            Assert.AreEqual(expectedResult, result);
+
+        }
+
+    }
+}
